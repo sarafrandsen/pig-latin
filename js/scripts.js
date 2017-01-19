7e8ba6e6779615d5e78cf50f0e454ffea1f9f35f
keely glenn and Sara Frandsen <keely@>
2017-01-18 16:46:16 -0800

    finally get it to recognize numbers vs letters

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
 var isLatin = function(word) {
-	return isNaN(word);
+	for (var i = 0; i < word.length; i += 1) {
+  	var letter = word[i];  // get the letter out of the string at index `i`
+  	if (!isNaN(letter)) {  // if it's NOT not-a-number, then it's a number... :\
+    	return false;  // return false right away because who cares about the rest?
+    }
+  }
+  // if we found no numbers at all, it's a STRING, BABY
+  return true;
 }
 
 $(document).ready(function() {
-	$("#english").submit(function(event) {
-  	event.preventDefault();
-    var word = $("#string").val();
-    var result = isLatin(word);
+	$("#english").submit(function(e) {
+  	e.preventDefault();  // none of your BS, form
+    var word = $("#string").val();  // get the word from the input
+    var result = isLatin(word);  // call `isLatin` with the word
     $("#pig").text(result);
   });
 });
